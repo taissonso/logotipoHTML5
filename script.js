545f5e76114900f59b68c43c28b7c2819f0a6ed8
taissonso <taisson.oliveira@gmail.com>
2019-10-01 23:22:24 -0300

    Função Javascript
    Função JavaScript para mostrar <HTML5> ao passar o mouse sobre o logo.

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrar() {
+    var texto = document.getElementsByClassName('texto')[0].style.display = 'block';
+}
+
+function esconder() {
+    document.getElementsByClassName('texto')[0].style.display = 'none';
+}
